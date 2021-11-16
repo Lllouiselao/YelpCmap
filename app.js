5a6c00c3f51f9d464a8967eb1982913903b129fe
LouisLao <louiselao64@gmail.com>
2021-11-16 01:49:58 -0800

    basic style of yelpcamp

--- app.js
+++ app.js
@@ -1,14 +1,76 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://localhost:27017/YelpCamp", {});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
 const app = express();
-const path = require('path');
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) =>{
-    res.render('home')
-})
+//how we tell texpress to pass the body back to us
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-app.listen(3000, () =>{
-    console.log('Serving on port 3000')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+// we need to tell the express to passed the body
+app.post("/campgrounds", async (req, res) => {
+  const userCampground = new Campground(req.body.campground);
+  await userCampground.save();
+  res.redirect(`/campgrounds/${userCampground._id}`);
+});
+
+//could click into the campground
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
+// could edit each campground
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+// we use a override method to fake update the form
+// then update the campground
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatecampground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${updatecampground._id}`);
+});
+
+// delete campground also use method override to fake a form 
+// make express thought it's a delete request
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect(`/campgrounds`);
+});
+
+app.listen(3000, () => {
+  console.log("Serving on port 3000");
+});
