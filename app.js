23b5fbcc6eee0cc44e30999a2015a24647a1d860
LouisLao <louiselao64@gmail.com>
2021-11-17 04:00:18 -0800

    style YelpCamp

--- app.js
+++ app.js
@@ -1,78 +1,71 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const ejsMate = require("ejs-mate");
-const methodOverride = require("method-override");
-const Campground = require("./models/campground");
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
 
-mongoose.connect("mongodb://localhost:27017/YelpCamp", {});
+mongoose.connect('mongodb://localhost:27017/YelpCamp')
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
+db.once("open", () =>{
+    console.log("app.js Database Connceted")
 });
 
 const app = express();
 
 app.engine('ejs', ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
 
-//how we tell texpress to pass the body back to us
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get('/', (req, res) =>{
+    res.render('home')
 });
 
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
+app.get('/campgrounds', async (req,res) =>{
+    const campgrounds = await Campground.find()
+    res.render('campgrounds/index', {campgrounds})
 });
 
-app.get("/campgrounds/new", (req, res) => {
-  res.render("campgrounds/new");
+app.get('/campgrounds/new', (req,res) =>{
+    res.render('campgrounds/new')
 });
 
-// we need to tell the express to passed the body
-app.post("/campgrounds", async (req, res) => {
-  const userCampground = new Campground(req.body.campground);
-  await userCampground.save();
-  res.redirect(`/campgrounds/${userCampground._id}`);
+//create a new campground
+app.post('/campgrounds', async(req, res) =>{
+    const newcampground = new Campground(req.body.campground);
+    await newcampground.save();
+    res.redirect(`/campgrounds/${newcampground._id}`)
 });
 
-//could click into the campground
-app.get("/campgrounds/:id", async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render("campgrounds/show", { campground });
+app.get('/campgrounds/:id', async(req, res) =>{
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
 });
 
-// could edit each campground
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render("campgrounds/edit", { campground });
-});
+app.get('/campgrounds/:id/edit', async(req, res) =>{
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground })
+})
 
-// we use a override method to fake update the form
-// then update the campground
-app.put("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const updatecampground = await Campground.findByIdAndUpdate(id, {
-    ...req.body.campground,
-  });
-  res.redirect(`/campgrounds/${updatecampground._id}`);
-});
+//edit the exsisting campground
+app.put('/campgrounds/:id', async(req, res) =>{
+    const {id} = req.params;
+    const updatecampground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${updatecampground._id}`)
+})
 
-// delete campground also use method override to fake a form 
-// make express thought it's a delete request
-app.delete("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect(`/campgrounds`);
-});
+//delete campground
+app.delete('/campgrounds/:id', async(req, res) =>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 
-app.listen(3000, () => {
-  console.log("Serving on port 3000");
+app.listen(3000, () =>{
+    console.log('serving on port 3000')
 });
--- seeds/index.js
+++ seeds/index.js
@@ -1,28 +1,34 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const cities = require('./cities');
-const {descriptors, places} = require("./seedHelpers")
-const Campground = require("../models/campground");
+const {places, descriptors} = require('./seedHelpers');
+const Campground = require('../models/campground');
 
-mongoose.connect("mongodb://localhost:27017/YelpCamp", {});
+mongoose.connect('mongodb://localhost:27017/YelpCamp')
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Seed Database connected");
-});
+db.once("open", () =>{
+    console.log("Seed.index Database Connceted")
+})
 
-const sample = array => array[Math.floor(Math.random() * array.length)]
+const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seeDB = async () => {
-  await Campground.deleteMany({});
-  for(let i = 0; i < 50; i++){
-      const random1000 = Math.floor(Math.random() * 1000);
-      const camp = new Campground({
+const seeDB = async() =>{
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-      })
-      await camp.save();
-  }
-};
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'The forest was humble, thick, and ancient. Its canopy was eclipsed by hazel, redwood, and linden, their crowns allowed cascading lights to shimmer through for a hodgepodge of mushrooms to rule the crunchy layer of leaves below.Curling tree limbs grasped many a tree, and a range of flowers, which desperately tried to claim the last remnants of light, clashed with the otherwise jade forest floor.',
+            image: 'https://source.unsplash.com/collection/973270',
+            price
+        })
+        await camp.save();
+    }
+}
 
-seeDB();
+seeDB().then(() =>{
+    mongoose.connection.close();
+})
